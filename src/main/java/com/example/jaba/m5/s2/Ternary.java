ad074fc504f7d729acd1189375125ffa00cee972
Emanuele <egalli64@gmail.com>
2024-09-27 22:03:39 +0200

    clarify

--- src/main/java/com/example/jaba/m5/s2/Ternary.java
+++ src/main/java/com/example/jaba/m5/s2/Ternary.java
@@ -15,20 +15,20 @@ public class Ternary {
      * Compared against if-else / default value + if
      */
     public static void main(String[] args) {
-        String version1 = args.length == 0 ? "No arg passed" : args[0];
+        String version1 = args.length == 0 ? "No argument passed" : args[0];
         System.out.println(version1);
 
         // Not nice: split declaration from conditional initialization
         String version2;
         if (args.length == 0) {
-            version2 = "No arg passed";
+            version2 = "No argument passed";
         } else {
             version2 = args[0];
         }
         System.out.println(version2);
 
         // Not nice: variable is defined, but sometimes immediately reassigned
-        String version3 = "No arg passed";
+        String version3 = "No argument passed";
         if (args.length != 0) {
             version3 = args[0];
         }
