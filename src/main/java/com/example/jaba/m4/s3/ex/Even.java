cd603b0057d9cc764d26b8ae0bcd3c556376be29
Emanuele <egalli64@gmail.com>
2024-09-29 10:31:52 +0200

    exercise

---
+++ src/main/java/com/example/jaba/m4/s3/ex/Even.java
@@ -0,0 +1,20 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m4.s3.ex;
+
+/**
+ * Exercise on an arithmetic operator
+ */
+public class Even {
+    public static void main(String[] args) {
+        int value = 42;
+
+        System.out.print(value);
+
+        // TODO: check if the value is even or odd
+        System.out.println(" is odd");
+    }
+}
