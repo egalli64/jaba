8a9939e3c52866dc82e7486bb535a061a156da5d
Emanuele <egalli64@gmail.com>
2024-10-02 20:54:00 +0200

    exercise

---
+++ src/main/java/com/example/jaba/m6/s3/ex/Exercise3.java
@@ -0,0 +1,29 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m6.s3.ex;
+
+import java.util.Arrays;
+
+/**
+ * Exercise: pass an array to a method and use it there
+ */
+public class Exercise3 {
+    public static void main(String[] args) {
+        int[] data = { 4, 3, 1, 3 };
+        System.out.println("The array: " + Arrays.toString(data));
+
+        // TODO: call increaseAll()
+        System.out.println("Now array is: " + Arrays.toString(data));
+    }
+
+    /**
+     * This method should get an int array as input and increase all its elements
+     * <p>
+     * TODO: Complete this method stub
+     */
+    static void increaseAll() {
+    }
+}
