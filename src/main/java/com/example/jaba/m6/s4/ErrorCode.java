46811046500298ebe4c15b8c96a160661b11ba28
Emanuele <egalli64@gmail.com>
2024-10-02 21:00:26 +0200

    clarify

--- src/main/java/com/example/jaba/m6/s4/ErrorCode.java
+++ src/main/java/com/example/jaba/m6/s4/ErrorCode.java
@@ -20,22 +20,22 @@ public class ErrorCode {
         double[] arguments = { 42.0, 0.0, -1.0 };
 
         // !!! unsafe !!!
-        for (double arg : arguments) {
-            double radix = Math.sqrt(arg);
+        for (double argument : arguments) {
+            double radix = Math.sqrt(argument);
             // We should not use the returned value without checking it!
-            System.out.println("Square root of " + arg + " is " + radix);
+            System.out.println("Square root of " + argument + " is " + radix);
         }
         System.out.println();
 
         // safer
-        for (double arg : arguments) {
-            double radix = Math.sqrt(arg);
+        for (double argument : arguments) {
+            double radix = Math.sqrt(argument);
 
             // Check the return value before using it!
             if (Double.isNaN(radix)) {
-                System.out.println("Can't calculate the square root of " + arg);
+                System.out.println("Can't calculate the square root of " + argument);
             } else {
-                System.out.println("Square root of " + arg + " is " + radix);
+                System.out.println("Square root of " + argument + " is " + radix);
             }
         }
     }
