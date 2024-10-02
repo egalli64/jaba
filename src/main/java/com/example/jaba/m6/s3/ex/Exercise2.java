408faf7ea6863076d385d0358e75f46188699e7f
Emanuele <egalli64@gmail.com>
2024-10-02 20:45:29 +0200

    exercise

---
+++ src/main/java/com/example/jaba/m6/s3/ex/Exercise2.java
@@ -0,0 +1,30 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m6.s3.ex;
+
+import java.util.Arrays;
+
+/**
+ * Exercise: pass an array to a method and use it there
+ */
+public class Exercise2 {
+    public static void main(String[] args) {
+        int[] data = { 4, 3, 1, 3 };
+        System.out.println("The array: " + Arrays.toString(data));
+
+        // TODO:
+        System.out.println("Sum of its first and last elements is ...");
+    }
+
+    /**
+     * This method should get an int array as input and return the sum of its and
+     * first element. Assume that the caller passes a "good" array
+     * <p>
+     * TODO: Complete this method stub
+     */
+    static void sumFirstLast() {
+    }
+}
