d2e928f67240c8a9146cacf7985070b1d6b18717
Emanuele <egalli64@gmail.com>
2024-10-03 12:18:04 +0200

    exercise

---
+++ src/main/java/com/example/jaba/m6/s7/ex/MaxMin.java
@@ -0,0 +1,28 @@
+/*
+ * Introduction to Java base concepts
+ * 
+ * https://github.com/egalli64/jaba
+ */
+package com.example.jaba.m6.s7.ex;
+
+/**
+ * Given two values print the smallest and the largest
+ * <p>
+ * Use Math.max() and Math.min()
+ */
+public class MaxMin {
+    public static void main(String[] args) {
+        int x = 42;
+        System.out.println("x is " + x);
+        int y = 53;
+        System.out.println("y is " + y);
+
+        // TODO: put in max the largest between x and y
+        int max = 0;
+        System.out.println("Max is " + max);
+
+        // TODO: put in min the smallest between x and y
+        int min = 0;
+        System.out.println("Min is " + min);
+    }
+}
